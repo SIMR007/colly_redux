937d5f27a367479578a4696042b069bb1e61dcf2
SIMR007 <simrandhiman762@gmail.com>
2024-01-17 17:55:00 +0530

    used  prettier

--- src/Pages/store/index.js
+++ src/Pages/store/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterBrandSlice from "./filterBrand-slice";
-import logger from 'redux-logger'
-const store = configureStore(
-  {
-    reducer: { filterBrands: filterBrandSlice.reducer },middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
-  }
-  
-);          // middleware provides the info about the actions and state in the console 
+import logger from "redux-logger";
+const store = configureStore({
+  reducer: { filterBrands: filterBrandSlice.reducer },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+}); // middleware provides the info about the actions and state in the console
 
 export default store;
